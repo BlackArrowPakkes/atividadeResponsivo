745ff510c8ce1ed2737ce95c16294836a4b948a3
BlackArrowPakkes <135661842+BlackArrowPakkes@users.noreply.github.com>
2023-06-22 13:44:49 -0300

    Add files via upload
    atividade respoNSIVO DO RODRIGO <3

---
+++ exemplo3.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade</title>
+    <link rel="stylesheet" type="text/css" href="estilo1.css">
+</head>
+<body>
+    <div class="topo"> 
+        <div class="logo"></div>
+        <div class="menu"></div>
+</div>
+        <div class="destaque">
+        </div>
+
+        <div class="corpo">
+            <div class="painel1"></div>
+            <div class="painel2"></div>
+            <div class="painel3"></div>
+            <div class="painel4"></div>
+        </div>
+
+        <div class="rodape"></div>
+
+</body>
+</html>
